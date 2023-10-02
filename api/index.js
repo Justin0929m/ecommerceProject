be5fe3c0ec4b8cfa8748df1958a35704d2ab65de
Justin0929m <scholtz0929@gmail.com>
2023-10-02 09:40:47 +0200

    Removed orders and users routes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,5 @@
 const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
-const userRoutes = require("./routes/userRoutes");
 const prodRoutes = require("./routes/prodRoutes");
-const orderRoutes = require("./routes/orderRoutes");
-const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const app = express();
@@ -12,10 +7,6 @@ const app = express();
 // Middleware
 app.use(
   express.json(),
-  session({ secret: "secret_key_test", resave: true, saveUninitialized: true }),
-  passport.initialize(),
-  passport.session(),
-  cookieParser(),
   cors()
 );
 
@@ -31,9 +22,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.use("/", userRoutes);
 app.use("/", prodRoutes);
-app.use("/", orderRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- api/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const orderController = require("../controllers/orderController");
-
-router.post("/order/:userID/:prodID", orderController.addOrder);
-router.get("/orders/:userID", orderController.fetchOrders);
-router.delete("/order/:orderID", orderController.deleteOrder);
-
-module.exports = router;
